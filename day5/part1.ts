e718ffaa2ffa0a1001d277bdd344974cb4ff623a
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-23 00:17:26 -0300

    fix(part1): Fixing it

--- day5/part1.ts
+++ day5/part1.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 
 const main = async () => {
   const data = readFileSync("./test.txt", { flag: "r" });
-  const diagram: number[] = [];
+  const diagram: number[][] = [];
   const coordinates = data
     .toString()
     .split("\n")
@@ -16,29 +16,32 @@ const main = async () => {
 
     if (x1 !== x2 && y1 !== y2) continue;
 
-    let range: number[] = [];
-    let stringify: (a: number) => number;
     let start: number = 0;
     let end: number = 0;
 
     if (y1 === y2) {
       [start, end] = x1 < x2 ? [x1, x2] : [x2, x1];
-      stringify = (a) => Number(`${a}${y1}`);
+      for (let j = start; j <= end; j++) {
+        if (!diagram[j]) diagram[j] = [];
+        diagram[j][y1] = (diagram[j][y1] || 0) + 1;
+      }
     } else {
       [start, end] = y1 < y2 ? [y1, y2] : [y2, y1];
-      stringify = (a) => Number(`${x1}${a}`);
-    }
-
-    for (let j = start; j <= end; j++) {
-      range.push(stringify(j));
-    }
-
-    for (let j = 0; j < range.length; j++) {
-      diagram[range[j]] = (diagram[range[j]] || 0) + 1;
+      for (let j = start; j <= end; j++) {
+        if (!diagram[x1]) diagram[x1] = [];
+        diagram[x1][j] = (diagram[x1][j] || 0) + 1;
+      }
     }
   }
 
-  console.log(diagram.filter((item) => item && item >= 2).length);
+  let sum = 0;
+  for (let i = 0; i < diagram.length; i++) {
+    for (let k = 0; k < diagram.length; k++) {
+      if (diagram[i] && diagram[i][k] && diagram[i][k] >= 2) sum++;
+    }
+  }
+
+  console.log(sum);
 };
 
 main();
