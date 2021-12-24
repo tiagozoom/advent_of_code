667d1359198b4c03a665fb369260bb2c83d4e78e
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-24 15:27:25 -0300

    fix(part2): Arithmetic numbers (good one)

--- day7/part2.ts
+++ day7/part2.ts
@@ -5,20 +5,24 @@ const main = async () => {
   const stream = createReadStream("./test.txt");
   const lines = readline.createInterface(stream);
   const crabs: number[] = [];
-  const { abs } = Math;
+  const { abs, max } = Math;
   let answer: number = 0;
 
   for await (let line of lines) {
     crabs.push(...line.split(",").map((item: string) => Number(item)));
   }
 
-  for (let i = 0; i < crabs.length; i++) {
-    let sum = 0;
-    for (let j = 0; j < crabs.length; j++) {
-      sum += abs(crabs[j] - crabs[i]);
-    }
+  const lastIndex = max(...crabs);
 
-    if (!answer || sum < answer) answer = sum;
+  for (let g = 0; g < lastIndex; g++) {
+    for (let i = 0; i < crabs.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < crabs.length; j++) {
+        const triangular = abs(crabs[j] - g);
+        sum += (triangular * (triangular + 1)) / 2;
+      }
+      if (!answer || answer > sum) answer = sum;
+    }
   }
 
   console.log(answer);
