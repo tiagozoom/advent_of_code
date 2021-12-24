c1f1d5ddd0d9dbcd31402741d39b415b1cc6e6d4
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-23 23:36:44 -0300

    feat(day6): Part1 is done

---
+++ day6/part1.ts
@@ -0,0 +1,26 @@
+import { createReadStream } from "fs";
+import * as readline from "readline";
+
+const main = async () => {
+  const stream = createReadStream("./test.txt");
+  const lines = readline.createInterface(stream);
+  const fishes: number[] = [];
+  const days: number = 80;
+  for await (let line of lines) {
+    fishes.push(...line.split(",").map((item) => Number(item)));
+  }
+
+  for (let i = 0; i < days; i++) {
+    for (let j = 0; j < fishes.length; j++) {
+      fishes[j] = fishes[j] - 1;
+      if (fishes[j] < 0) {
+        fishes[j] = 6;
+        fishes.push(9);
+      }
+    }
+  }
+
+  console.log(fishes.length);
+};
+
+main();
---
+++ day6/part2.ts
@@ -0,0 +1,41 @@
+import { createReadStream } from "fs";
+import * as readline from "readline";
+
+const main = async () => {
+  const stream = createReadStream("./test.txt");
+  const lines = readline.createInterface(stream);
+  const { max, abs } = Math;
+  const coordinates: number[][] = [];
+
+  for await (let line of lines) {
+    const [start, end] = line.toString().split(" -> ");
+    const [x1, y1] = start.split(",").map((item) => Number(item));
+    const [x2, y2] = end.split(",").map((item) => Number(item));
+
+    const h = x2 - x1;
+    const v = y2 - y1;
+
+    const distance = max(abs(h), abs(v));
+
+    for (let i = 0; i <= distance; i++) {
+      const dx = x1 + (h === 0 ? 0 : h > 0 ? 1 : -1) * i;
+      const dy = y1 + (v === 0 ? 0 : v > 0 ? 1 : -1) * i;
+
+      if (!coordinates[dx]) coordinates[dx] = [];
+      coordinates[dx][dy] = (coordinates[dx][dy] || 0) + 1;
+    }
+  }
+
+  let sum = 0;
+  const array = coordinates.filter((item) => item);
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] >= 2) sum++;
+    }
+  }
+
+  console.log(sum);
+};
+
+main();
