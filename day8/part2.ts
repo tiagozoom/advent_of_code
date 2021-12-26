518439a4f2675569ed4c9a3828164aa6352046f6
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-26 12:57:34 -0300

    fix(part2): Not the best solution but we are getting there

--- day8/part2.ts
+++ day8/part2.ts
@@ -5,75 +5,62 @@ const main = async () => {
   const stream = createReadStream("./test.txt");
   const lines = readline.createInterface(stream);
   let sum = 0;
+  let zero: string[] = [];
+  let one: string[] = [];
+  let two: string[] = [];
+  let three: string[] = [];
+  let four: string[] = [];
+  let five: string[] = [];
+  let six: string[] = [];
+  let nine: string[] = [];
+  let numbers: string[] = [];
+  const lengthSix: string[][] = [];
+  const lengthFive: string[][] = [];
 
   for await (let line of lines) {
-    const [chars, numbers] = line.split(" | ");
-    let words = chars.split(" ");
-    let zero: string[] = [];
-    let one: string[] = [];
-    let two: string[] = [];
-    let three: string[] = [];
-    let four: string[] = [];
-    let five: string[] = [];
-    let six: string[] = [];
-    let nine: string[] = [];
+    const [chars, rest] = line.split(" | ");
+    const words = chars.split(" ");
+    numbers = rest.split(" ");
     let answer = "";
 
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      if (word.length === 2) {
-        one = word.split("").sort();
-        delete words[i];
-      } else if (word.length === 4) {
-        four = word.split("").sort();
-        delete words[i];
-      }
+    for (let word of words) {
+      if (word.length === 2) one = word.split("").sort();
+      else if (word.length === 4) four = word.split("").sort();
+      else if (word.length === 6) lengthSix.push(word.split("").sort());
+      else if (word.length === 5) lengthFive.push(word.split("").sort());
     }
 
-    words = words.filter((item) => item);
-
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      if (word.length === 5) {
-        const unknown = word.split("").sort();
-        let loses = 0;
-        let isThree = true;
-        for (let i = 0; i < one.length; i++) {
-          if (!unknown.includes(one[i])) isThree = false;
-        }
-        for (let i = 0; i < four.length; i++) {
-          if (!unknown.includes(four[i])) loses++;
-        }
-        if (isThree) three = unknown;
-        else if (loses === 1) five = unknown;
-        else two = unknown;
-        delete words[i];
+    for (let unknown of lengthFive) {
+      let loses = 0;
+      let isThree = true;
+      for (let i = 0; i < one.length; i++) {
+        if (!unknown.includes(one[i])) isThree = false;
       }
+      for (let i = 0; i < four.length; i++) {
+        if (!unknown.includes(four[i])) loses++;
+      }
+      if (isThree) three = unknown;
+      else if (loses === 1) five = unknown;
+      else two = unknown;
     }
 
-    words = words.filter((item) => item);
+    for (let unknown of lengthSix) {
+      let isNine = true;
+      let isSix = true;
 
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      if (word.length === 6) {
-        const unknown = word.split("").sort();
-        let isNine = true;
-        let isSix = true;
-        for (let i = 0; i < three.length; i++) {
-          if (!unknown.includes(three[i])) isNine = false;
-        }
-        for (let i = 0; i < five.length; i++) {
-          if (!unknown.includes(five[i])) isSix = false;
-        }
-
-        if (isNine) nine = unknown;
-        else if (isSix) six = unknown;
-        else zero = unknown;
-        delete words[i];
+      for (let i = 0; i < three.length; i++) {
+        if (!unknown.includes(three[i])) isNine = false;
       }
+      for (let i = 0; i < five.length; i++) {
+        if (!unknown.includes(five[i])) isSix = false;
+      }
+
+      if (isNine) nine = unknown;
+      else if (isSix) six = unknown;
+      else zero = unknown;
     }
 
-    for (let number of numbers.split(" ")) {
+    for (let number of numbers) {
       const unknown = number.split("").sort();
       if (unknown.length === 4) answer = `${answer}4`;
       else if (unknown.length === 2) answer = `${answer}1`;
@@ -104,7 +91,6 @@ const main = async () => {
         else answer = `${answer}9`;
       }
     }
-
     sum += Number(answer);
   }
 
