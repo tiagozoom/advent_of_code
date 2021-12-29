7d4580adbe94ab4867725e84e0e3df2369fcff14
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-29 18:20:27 -0300

    feat(part2): Reasonable solution

--- day10/part2.ts
+++ day10/part2.ts
@@ -36,57 +36,48 @@ class Stack<T> implements IStack<T> {
 const main = async () => {
   const stream = createReadStream("./test.txt");
   const lines = readline.createInterface(stream);
-  const matrix: number[][] = [];
-  const basins: number[] = [];
-  const visited: boolean[][] = [];
+  const opens: { [key: string]: string } = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+    "<": ">",
+  };
+  let errorSum: number[] = [];
 
-  let i = 0;
   for await (let line of lines) {
-    const values = line.split("").map((item) => Number(item));
-    matrix[i] = values;
-    i++;
-  }
+    const stack = new Stack<string>();
+    const values = line.split("");
+    let errorFound = false;
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (!visited[i]) visited[i] = [];
-      visited[i][j] = false;
-    }
-  }
-
-  // Right, Down, Left, Up
-  const xDirections = [1, 0, -1, 0];
-  const yDirections = [0, -1, 0, 1];
-
-  const stack = new Stack<[number, number]>();
-
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === 9 || visited[i][j]) continue;
-      stack.push([i, j]);
-      let basin = 1;
-      while (stack.size()) {
-        const [x, y] = stack.pop()!;
-        visited[x][y] = true;
-        for (let k = 0; k < xDirections.length; k++) {
-          const xx = x + xDirections[k];
-          const yy = y + yDirections[k];
-          const xValid = xx >= 0 && xx < matrix.length;
-          const yValid = yy >= 0 && yy < matrix[x].length;
-          if (xValid && yValid && matrix[xx][yy] !== 9 && !visited[xx][yy]) {
-            stack.push([xx, yy]);
-            visited[xx][yy] = true;
-            basin++;
-          }
+    for (let i = 0; i < values.length; i++) {
+      if (opens[values[i]]) stack.push(values[i]);
+      else {
+        const close = stack.pop()!;
+        if (close && values[i] !== opens[close]) {
+          errorFound = true;
         }
       }
-      basins.push(basin);
+    }
+
+    if (!errorFound) {
+      let score = 0;
+      while (stack.size()) {
+        const value = stack.pop()!;
+        let completionValue = 0;
+        if (opens[value] === ")") completionValue = 1;
+        else if (opens[value] === "]") completionValue = 2;
+        else if (opens[value] === "}") completionValue = 3;
+        else if (opens[value] === ">") completionValue = 4;
+        score = score * 5 + completionValue;
+      }
+
+      errorSum.push(score);
     }
   }
 
-  const ordered = basins.sort((a, b) => (a < b ? 1 : -1));
+  const ordered = errorSum.sort((a, b) => (a > b ? 1 : -1));
 
-  console.log(ordered[0] * ordered[1] * ordered[2]);
+  console.log(ordered[Math.floor(ordered.length / 2)]);
 };
 
 main();
