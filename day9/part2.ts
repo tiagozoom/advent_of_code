3ec0e747e2351381cd2376dd4ec90b98b5fdcbe3
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-29 15:32:04 -0300

    feat(part1): Not working yet

--- day9/part2.ts
+++ day9/part2.ts
@@ -4,94 +4,58 @@ import * as readline from "readline";
 const main = async () => {
   const stream = createReadStream("./test.txt");
   const lines = readline.createInterface(stream);
-  let sum = 0;
-  let zero: string[] = [];
-  let one: string[] = [];
-  let two: string[] = [];
-  let three: string[] = [];
-  let four: string[] = [];
-  let five: string[] = [];
-  let six: string[] = [];
-  let nine: string[] = [];
-  let numbers: string[] = [];
-  const lengthSix: string[][] = [];
-  const lengthFive: string[][] = [];
+  const matrix: number[][] = [];
+  const basins: number[] = [];
+  let sum = 1;
 
+  const findBasin = (
+    points: number[][],
+    x: number,
+    y: number,
+    found: { [key: string]: number } = {}
+  ): number => {
+    const nextPoint = matrix[x][y] + 1;
+    found[`${x}${y}`] = 1;
+    if (
+      x + 1 < points.length &&
+      points[x + 1][y] === points[x][y] + 1 &&
+      points[x + 1][y] < 9
+    ) {
+      findBasin(points, x + 1, y, found);
+    }
+    if (x - 1 >= 0 && points[x - 1][y] === nextPoint && points[x - 1][y] < 9) {
+      findBasin(points, x - 1, y, found);
+    }
+    if (y - 1 >= 0 && points[x][y - 1] === nextPoint && points[x][y - 1] < 9) {
+      findBasin(points, x, y - 1, found);
+    }
+    if (
+      y + 1 < points[x].length &&
+      points[x][y + 1] === nextPoint &&
+      points[x][y + 1] < 9
+    ) {
+      findBasin(points, x, y + 1, found);
+    }
+    return Object.keys(found).length;
+  };
+
+  let i = 0;
   for await (let line of lines) {
-    const [chars, rest] = line.split(" | ");
-    const words = chars.split(" ");
-    numbers = rest.split(" ");
-    let answer = "";
+    const values = line.split("").map((item) => Number(item));
+    matrix[i] = values;
+    i++;
+  }
 
-    for (let word of words) {
-      if (word.length === 2) one = word.split("").sort();
-      else if (word.length === 4) four = word.split("").sort();
-      else if (word.length === 6) lengthSix.push(word.split("").sort());
-      else if (word.length === 5) lengthFive.push(word.split("").sort());
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] < 9) basins.push(findBasin(matrix, i, j));
     }
+  }
 
-    for (let unknown of lengthFive) {
-      let loses = 0;
-      let isThree = true;
-      for (let i = 0; i < one.length; i++) {
-        if (!unknown.includes(one[i])) isThree = false;
-      }
-      for (let i = 0; i < four.length; i++) {
-        if (!unknown.includes(four[i])) loses++;
-      }
-      if (isThree) three = unknown;
-      else if (loses === 1) five = unknown;
-      else two = unknown;
-    }
+  const ordered = basins.sort((a, b) => (a < b ? 1 : -1));
 
-    for (let unknown of lengthSix) {
-      let isNine = true;
-      let isSix = true;
-
-      for (let i = 0; i < three.length; i++) {
-        if (!unknown.includes(three[i])) isNine = false;
-      }
-      for (let i = 0; i < five.length; i++) {
-        if (!unknown.includes(five[i])) isSix = false;
-      }
-
-      if (isNine) nine = unknown;
-      else if (isSix) six = unknown;
-      else zero = unknown;
-    }
-
-    for (let number of numbers) {
-      const unknown = number.split("").sort();
-      if (unknown.length === 4) answer = `${answer}4`;
-      else if (unknown.length === 2) answer = `${answer}1`;
-      else if (unknown.length === 3) answer = `${answer}7`;
-      else if (unknown.length === 7) answer = `${answer}8`;
-      else {
-        let isThree = true;
-        let isTwo = true;
-        let isFive = true;
-        let isSix = true;
-        let isNine = true;
-        let isZero = true;
-
-        for (let i = 0; i < unknown.length; i++) {
-          if (!unknown.includes(zero[i])) isZero = false;
-          if (!unknown.includes(two[i])) isTwo = false;
-          if (!unknown.includes(three[i])) isThree = false;
-          if (!unknown.includes(five[i])) isFive = false;
-          if (!unknown.includes(six[i])) isSix = false;
-          if (!unknown.includes(nine[i])) isNine = false;
-        }
-
-        if (isZero) answer = `${answer}0`;
-        else if (isTwo) answer = `${answer}2`;
-        else if (isThree) answer = `${answer}3`;
-        else if (isFive) answer = `${answer}5`;
-        else if (isSix) answer = `${answer}6`;
-        else answer = `${answer}9`;
-      }
-    }
-    sum += Number(answer);
+  for (let i = 0; i < ordered.length && i <= 2; i++) {
+    sum *= ordered[i];
   }
 
   console.log(sum);
