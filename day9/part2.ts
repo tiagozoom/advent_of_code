d50585b6464c0da818dec852da9544615e01318a
tiago.turibio <tiago.turibio@olxbr.com>
2021-12-29 17:18:38 -0300

    fix(part2): Worked with BFS

--- day9/part2.ts
+++ day9/part2.ts
@@ -1,43 +1,44 @@
 import { createReadStream } from "fs";
 import * as readline from "readline";
 
+interface IStack<T> {
+  push(item: T): void;
+  pop(): T | undefined;
+  peek(): T | undefined;
+  size(): number;
+}
+
+class Stack<T> implements IStack<T> {
+  private storage: T[] = [];
+
+  constructor(private capacity: number = Infinity) {}
+
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("Stack has reached max capacity, you cannot add more items");
+    }
+    this.storage.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
+
+  peek(): T | undefined {
+    return this.storage[this.size() - 1];
+  }
+
+  size(): number {
+    return this.storage.length;
+  }
+}
+
 const main = async () => {
   const stream = createReadStream("./test.txt");
   const lines = readline.createInterface(stream);
   const matrix: number[][] = [];
   const basins: number[] = [];
-  let sum = 1;
-
-  const findBasin = (
-    points: number[][],
-    x: number,
-    y: number,
-    found: { [key: string]: number } = {}
-  ): number => {
-    const nextPoint = matrix[x][y] + 1;
-    found[`${x}${y}`] = 1;
-    if (
-      x + 1 < points.length &&
-      points[x + 1][y] === points[x][y] + 1 &&
-      points[x + 1][y] < 9
-    ) {
-      findBasin(points, x + 1, y, found);
-    }
-    if (x - 1 >= 0 && points[x - 1][y] === nextPoint && points[x - 1][y] < 9) {
-      findBasin(points, x - 1, y, found);
-    }
-    if (y - 1 >= 0 && points[x][y - 1] === nextPoint && points[x][y - 1] < 9) {
-      findBasin(points, x, y - 1, found);
-    }
-    if (
-      y + 1 < points[x].length &&
-      points[x][y + 1] === nextPoint &&
-      points[x][y + 1] < 9
-    ) {
-      findBasin(points, x, y + 1, found);
-    }
-    return Object.keys(found).length;
-  };
+  const visited: boolean[][] = [];
 
   let i = 0;
   for await (let line of lines) {
@@ -48,17 +49,44 @@ const main = async () => {
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] < 9) basins.push(findBasin(matrix, i, j));
+      if (!visited[i]) visited[i] = [];
+      visited[i][j] = false;
+    }
+  }
+
+  // Right, Down, Left, Up
+  const xDirections = [1, 0, -1, 0];
+  const yDirections = [0, -1, 0, 1];
+
+  const stack = new Stack<[number, number]>();
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 9 || visited[i][j]) continue;
+      stack.push([i, j]);
+      let basin = 1;
+      visited[i][j] = true;
+      while (stack.size()) {
+        const [x, y] = stack.pop()!;
+        for (let k = 0; k < xDirections.length; k++) {
+          const xx = x + xDirections[k];
+          const yy = y + yDirections[k];
+          const xValid = xx >= 0 && xx < matrix.length;
+          const yValid = yy >= 0 && yy < matrix[x].length;
+          if (xValid && yValid && matrix[xx][yy] !== 9 && !visited[xx][yy]) {
+            stack.push([xx, yy]);
+            visited[xx][yy] = true;
+            basin++;
+          }
+        }
+      }
+      basins.push(basin);
     }
   }
 
   const ordered = basins.sort((a, b) => (a < b ? 1 : -1));
 
-  for (let i = 0; i < ordered.length && i <= 2; i++) {
-    sum *= ordered[i];
-  }
-
-  console.log(sum);
+  console.log(ordered[0] * ordered[1] * ordered[2]);
 };
 
 main();
